2eda238b4ee3b607395231895d38d8adb304f9d6
ram-221 <rameshmalli857.com>
2022-07-13 14:30:47 +0530

    calculating wages till condition

--- bridgelabday03uc/EmployeeWageUC6.java
+++ bridgelabday03uc/EmployeeWageUC6.java
@@ -1,33 +1,40 @@
-//Calculating Wages for a Month assuming 20 Working Days in a Month
+//Calculate Wages till a condition of total working hours of 100 or max days os 20 is reached for a month
+
 package com.bridgelabday03uc;
 
-public class EmployeeWageUC5 {
+public class EmployeeWageUC6 {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=2;
+
 	public static void main(String[] args) {
 		//Variables
-		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		//Computation
-		for(int day=0;day< NUM_OF_WORKING_DAYS;day++) {
+		int empHrs=0, empWage=0, totalEmpWage=0;
+
+		//computation
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
-			switch (empCheck) {
+
+			switch(empCheck) {
+
 			case IS_PART_TIME:
-				empHrs=0;
+				empHrs=4;
 				break;
+
 			case IS_FULL_TIME:
 				empHrs=8;
 				break;
+
 			default:
 				empHrs=0;
+
 			}
 			empWage=empHrs*EMP_RATE_PER_HOUR;
 			totalEmpWage+=empWage;
 			System.out.println("Emp Wage:"+empWage);
 		}
-		System.out.println("Total Emp Wage:"+totalEmpWage);
+		System.out.println("Total Emp Eage"+totalEmpWage);
 	}
 }
